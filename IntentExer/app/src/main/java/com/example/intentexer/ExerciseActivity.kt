437c6a37849e8fd7cbd4fa786c68f685f7bbaf07
Sophie <tengxxu@gmail.com>
2021-06-10 14:23:00 -0400

    add function to new activity

--- IntentExer/app/src/main/java/com/example/intentexer/ExerciseActivity.kt
+++ IntentExer/app/src/main/java/com/example/intentexer/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intentexer
 
+import android.content.Intent
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +9,26 @@ class ExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
+        loadImage(intent)
+    }
 
-        val imageUrl = intent.getStringExtra("image_url")
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let {
+            loadImage(intent)
+        }
+    }
+
+    private fun loadImage(intent: Intent) {
+        val action = intent.action
+        val data = intent.dataString
+
+        val imageUrl = if (action == Intent.ACTION_VIEW && data != null) data
+        else intent.getStringExtra("image_url")
 
         val webView = findViewById<WebView>(R.id.web_view)
-        imageUrl?.let{
+        imageUrl?.let {
             webView.loadUrl(it)
         }
-
     }
 }
