b0d9f691b715c945b4c6f44b26a9a39877729e42
Sophie <tengxxu@gmail.com>
2021-06-24 20:26:03 -0400

    Add: SavedStateHandle

--- Assignments/Assignment 2/DogViewerApplication/app/src/main/java/com/kim/deryk/byung/dogviewerapplication/ui/details/ImageActivity.kt
+++ Assignments/Assignment 2/DogViewerApplication/app/src/main/java/com/kim/deryk/byung/dogviewerapplication/ui/details/ImageActivity.kt
@@ -10,15 +10,14 @@ import com.kim.deryk.byung.dogviewerapplication.DogViewerApplication
 import com.kim.deryk.byung.dogviewerapplication.R
 import com.kim.deryk.byung.dogviewerapplication.data.DogRepository
 import com.kim.deryk.byung.dogviewerapplication.data.network.models.Breed
+import com.kim.deryk.byung.dogviewerapplication.data.network.models.BreedDetails
 
 class ImageActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<ImageViewModel> {
         val apiService = (application as DogViewerApplication).serviceLocator.apiService
         val repository = DogRepository(apiService)
-        //val dogBreedName = intent.getStringExtra(BundleKeys.DOG_BREED_NAME) ?: ""
-        val dogBreed = intent.getSerializableExtra(BundleKeys.DOG_BREED) ?: Breed("", "")
-        ImageViewModelFactory(repository, dogBreed as Breed)
+        ImageViewModelFactory(repository, this, intent.extras)
 
     }
 
@@ -26,14 +25,15 @@ class ImageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
 
-        //val dogBreedNameString = intent.getStringExtra(BundleKeys.DOG_BREED_NAME) ?: ""
-        //val dogBreed = intent.getStringExtra(BundleKeys.DOG_BREED_NAME) ?: ""
-
-        val dogBreedName: TextView = findViewById(R.id.dog_breed_name)
-        //dogBreedName.text = dogBreedNameString
         viewModel.dogBreedDetails.observe(this){
             Log.e("txx3", it.toString())
-            dogBreedName.text = it.url
+            populate(it)
         }
     }
+
+    private fun populate(breedDetails: BreedDetails){
+        val dogBreedName: TextView = findViewById(R.id.dog_breed_name)
+        dogBreedName.text = breedDetails.url
+    }
 }
+
--- Assignments/Assignment 2/DogViewerApplication/app/src/main/java/com/kim/deryk/byung/dogviewerapplication/ui/details/ImageViewModel.kt
+++ Assignments/Assignment 2/DogViewerApplication/app/src/main/java/com/kim/deryk/byung/dogviewerapplication/ui/details/ImageViewModel.kt
@@ -1,9 +1,11 @@
 package com.kim.deryk.byung.dogviewerapplication.ui.details
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import android.os.Bundle
+import android.telecom.Call
+import androidx.lifecycle.*
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryOwner
+import com.kim.deryk.byung.dogviewerapplication.BundleKeys
 import com.kim.deryk.byung.dogviewerapplication.data.DogRepository
 import com.kim.deryk.byung.dogviewerapplication.data.network.models.Breed
 import kotlinx.coroutines.launch
@@ -11,34 +13,38 @@ import kotlinx.coroutines.launch
 
 class ImageViewModel(
     private val repository: DogRepository,
-    //private val dogBreedName: String
-    private val breed: Breed,
+    private val handle: SavedStateHandle
 ): ViewModel(){
     val dogBreedDetails = repository.dogBreedDetails
+    val breed: Breed = handle.get<Breed>(BundleKeys.DOG_BREED) ?: Breed("", "")
+
     init {
         viewModelScope.launch {
-            //repository.loadDogBreedDetails(dogBreedName)
             repository.loadDogBreedDetails(breed)
         }
     }
 }
 
+
 class ImageViewModelFactory(
     private val repository: DogRepository,
-    //private val dogBreedName: String
-    private val breed: Breed
-    ): ViewModelProvider.Factory {
-
+    owner: SavedStateRegistryOwner,
+    defaultArgs: Bundle?
+): AbstractSavedStateViewModelFactory(owner, defaultArgs){
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel?> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
         if (modelClass.isAssignableFrom(ImageViewModel::class.java)) {
-            //return ImageViewModel(repository, dogBreedName) as T
-            return ImageViewModel(repository, breed) as T
+            return ImageViewModel(repository, handle) as T
+
         }
         throw IllegalArgumentException("Invalid ViewModel")
     }
-
 }
+
 
 
 
